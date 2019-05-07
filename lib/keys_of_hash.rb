64c45bfa504873ded1140f77eca5c6199724e80d
James Britton <brittonjp@gmail.com>
2019-05-07 19:14:28 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -11,3 +11,19 @@ class Hash
     new_array
   end
 end
+
+=begin
+class Hash
+  def keys_of(*arguments)
+    array = []
+    arguments.each do |arg|
+      self.each do |k, v|
+        if v == arg
+          array << k
+        end
+      end
+    end
+    array
+  end
+end
+=end
