d7ef1f7f42c63210dddb8a2ccc37f0ba3fdc32c1
James Britton <brittonjp@gmail.com>
2019-05-07 18:57:15 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,14 +3,12 @@ require 'pry'
 class Hash
   def keys_of(*arguments)
     array = []
-    arguments.each do |arg|
-      self.each do |k, v|
+    self.each do |k, v|
         #if v == arg
-        if arg.include?(v)
+        if arguments.include?(v)
           array << k
         end
       end
     end
     array
-  end
 end
